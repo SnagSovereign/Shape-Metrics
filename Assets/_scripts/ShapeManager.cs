a3f069b697b021ed372a86ebebd429cac812847b
SnagSovereign <joeysimon@outlook.com>
2021-02-25 12:13:22 +1000

    initial commit

---
+++ Assets/_scripts/ShapeManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShapeManager : MonoBehaviour {
+
+	[SerializeField] Text perimeterText, areaText, volumeText, surfaceAreaText;
+
+
+
+	//	BEGIN Square Metrics
+	//		GET length of side
+	//		CALCULATE perimeter = 4 * length
+	//		CALCULATE area = length squared
+	//		CALCULATE volume = length cubed
+	//		CALCULATE totalSurfaceArea = 6 * area
+	//		DISPLAY perimeter
+	//		DISPLAY area
+	//		DISPLAY volume
+	//		DISPLAY totalSurfaceArea
+	//	END
+}
